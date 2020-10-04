bea0d3863c33bfb9f07210ba75a4683315043939
benjaminsage <benjaminlsage@gmail.com>
2020-10-04 16:42:54 -0700

    Updated PageControl to be able to edit the current page

--- Sources/PageView/PageView.swift
+++ Sources/PageView/PageView.swift
@@ -111,6 +111,10 @@ fileprivate struct PageControl: UIViewRepresentable {
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
         control.numberOfPages = numberOfPages
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.updateCurrentPage(sender:)),
+            for: .valueChanged)
         
         return control
     }
