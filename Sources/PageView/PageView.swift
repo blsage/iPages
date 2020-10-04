f8a891b5d4c6393856a0fae6c516c62daaf67573
benjaminsage <benjaminlsage@gmail.com>
2020-10-03 17:58:10 -0700

    Initial commit

---
+++ Sources/PageView/PageView.swift
@@ -0,0 +1,126 @@
+import SwiftUI
+import UIKit
+
+@available(iOS 13.0, *)
+public struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+    @Binding var currentPage: Int
+    var showsPageControl: Bool
+    
+    init(_ views: [Page], currentPage: Binding<Int>, showsPageControl: Bool = true) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+        self._currentPage = currentPage
+        self.showsPageControl = showsPageControl
+    }
+    
+    public var body: some View {
+        ZStack(alignment: .bottom) {
+            PageViewController(controllers: viewControllers, currentPage: $currentPage)
+            if showsPageControl {
+                PageControl(numberOfPages: viewControllers.count, currentPage: $currentPage)
+                    .padding()
+            }
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+fileprivate struct PageViewController: UIViewControllerRepresentable {
+    var controllers: [UIViewController]
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
+        pageViewController.delegate = context.coordinator
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [controllers[currentPage]], direction: .forward, animated: true)
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+        var parent: PageViewController
+        
+        init(_ pageViewController: PageViewController) {
+            self.parent = pageViewController
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return parent.controllers.last
+            }
+            return parent.controllers[index - 1]
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == parent.controllers.count {
+                return parent.controllers.first
+            }
+            return parent.controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = parent.controllers.firstIndex(of: visibleViewController)
+            {
+                parent.currentPage = index
+            }
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+fileprivate struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        
+        return control
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
