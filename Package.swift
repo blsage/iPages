3e49dd6afa067c83d5ce38314874f59a4297f83c
benjaminsage <benjaminlsage@gmail.com>
2020-10-25 16:52:14 -0700

    Added Quintero dependency

--- Package.swift
+++ Package.swift
@@ -19,14 +19,15 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/benjaminsage/iGraphics.git", from: "0.0.5"),
-        .package(url: "https://github.com/benjaminsage/iColor.git", from: "0.1.0")
+        .package(url: "https://github.com/benjaminsage/iColor.git", from: "0.1.0"),
+        .package(url: "https://github.com/nerdsupremacist/AssociatedTypeRequirementsKit", from: "0.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iPages",
-            dependencies: ["iGraphics", "iColor"]),
+            dependencies: ["iGraphics", "iColor", "AssociatedTypeRequirementsKit"]),
         .testTarget(
             name: "iPagesTests",
             dependencies: ["iPages"]),
