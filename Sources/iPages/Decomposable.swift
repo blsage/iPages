7c1ca815d15f28916673583a673d72181100be09
benjaminsage <benjaminlsage@gmail.com>
2020-10-25 17:43:12 -0700

    Renamed Decomposable

---
+++ Sources/iPages/Decomposable.swift
@@ -0,0 +1,45 @@
+//
+//  Decomposable.swift
+//  
+//
+//  Created by Benjamin Sage on 10/21/20.
+//
+
+import SwiftUI
+
+protocol Decomposable {
+    func subviews() -> [AnyView]
+}
+
+extension View {
+    static func any(from view: Any) -> AnyView {
+        return AnyView(view as! Self)
+    }
+}
+
+extension View {
+    func decompose() -> [AnyView] {
+        if let decomposable = self as? Decomposable {
+            return decomposable.subviews()
+        }
+        if Body.self == Never.self {
+            return [AnyView(self)]
+        }
+        return body.decompose()
+    }
+}
+
+extension ForEach: Decomposable where Content: View {
+    func subviews() -> [AnyView] {
+        return data.map(content).flatMap { $0.decompose() }
+    }
+}
+
+extension TupleView: Decomposable {
+    func subviews() -> [AnyView] {
+        let mirror = Mirror(reflecting: self)
+        let tuple = mirror.children.first!.value
+        let tupleMirror = Mirror(reflecting: tuple)
+        return tupleMirror.children.map { AnyView(_fromValue: $0.value)! }
+    }
+}
